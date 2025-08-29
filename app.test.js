8077f965f3a4d774df4545bf1b403028a23e28aa
André Luís Bordignon <albordignon@gmail.com>
2025-08-28 21:27:32 -0300

    lkj

---
+++ app.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const app = require("../app"); // o aluno deve exportar o app
+
+describe("Testes automáticos - Produtos", () => {
+  // it("Deve criar um produto", async () => {
+  //   const res = await request(app).post("/produtos").send({ nome: "Café" });
+  //   expect(res.statusCode).toBe(201);
+  //   expect(res.body.nome).toBe("Café");
+  // });
+
+  it("Deve listar produtos", async () => {
+    const res = await request(app).get("/produtos");
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+});
