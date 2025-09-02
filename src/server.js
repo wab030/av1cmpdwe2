4ab069d795d32c5f11a0bfc0f76d9454a651a889
André Luís Bordignon <albordignon@gmail.com>
2025-09-02 15:23:57 -0300

    Teste do workflow do github

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor certinho rodando em http://localhost:${PORT}`);
 });
