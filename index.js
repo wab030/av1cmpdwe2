6ad617b52891adb0ad4615f1e66da7d7a11aa6ae
André Luís Bordignon <albordignon@gmail.com>
2025-09-22 09:56:14 -0300

    teste2

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api', livroRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na portaaaaa ${PORT}`);
 });
