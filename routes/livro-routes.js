d67a8e370b91fe1a9f202d6366e252a32cba3953
André Luís Bordignon <albordignon@gmail.com>
2025-09-22 10:02:17 -0300

    Nova versão do repo de correção

--- routes/livro-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const livroController = require('../controllers/LivroController');
-
-// Define a rota GET /books para listar os livros disponíveis
-router.get('/books', livroController.listarLivrosDisponiveis);
-
-module.exports = router;
