94fd6df5b63c91f46494186af7208b30aa99764f
André Luís Bordignon <albordignon@gmail.com>
2025-09-26 11:57:01 -0300

    Teste como aluno

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const app = require('./app');
+const app = require('./app'); // Importa a aplicação Express
+const PORT = 5000; // Define a porta obrigatória
 
-const PORT = 4040;
+// Inicia o servidor na porta 5000
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
