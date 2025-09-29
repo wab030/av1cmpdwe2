26e169d6891c45c89cbf2264c31073905f7494e6
André Luís Bordignon <albordignon@gmail.com>
2025-09-29 15:05:59 -0300

    Add 2 arquivos principais.

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const livroRoutes = require('./routes/livroRoutes');
+
+// Configuração do EJS como motor de visualização
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+// Middlewares
+app.use(express.urlencoded({ extended: true })); // Para parsear dados de formulário
+app.use(express.json()); // Para parsear JSON
+app.use(express.static('public')); // Servir arquivos estáticos
+
+// Rotas da Aplicação
+app.use('/', livroRoutes);
+console.log('ewt');
+
+// Middleware de tratamento de erros global
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    // Para erros não tratados ou de DB
+    res.status(500).render('error', { 
+        message: "Erro interno do servidor. Tente novamente mais tarde." 
+    });
+});
+
+// Exporta a instância do aplicativo para ser usada pelo server.js e pelos testes!
+module.exports = app;
