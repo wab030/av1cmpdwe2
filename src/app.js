fe4de7cde89297df268be86e9533c548210e3f2e
André Luís Bordignon <albordignon@gmail.com>
2025-09-28 22:00:35 -0300

    Finalmente

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     // Para erros não tratados ou de DB
     res.status(500).render('error', { 
-        message: "Erro interno do servidor. Tente novamente mais tarde." 
+        message: "Erro interno do se        rvidor. Tente novamente mais tarde." 
     });
 });
 
